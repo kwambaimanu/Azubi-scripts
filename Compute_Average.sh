82745bcc396f9a54a8a386d807de57cfde806e24
kwambaimanu@gmail.com <kwambaimanu@gmail.com>
2022-05-21 01:13:45 +0300

    Comparing Numbers

---
+++ Compute_Average.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+#https://github.com/kwambaimanu/Computing-average/blob/main/Compute_Average.sh
+#shell script to find average of n numbers
+  
+echo "Enter Size(N)"
+read N
+
+i=1
+sum=0
+
+echo "Enter Numbers"
+while [ $i -le $N ]
+do
+  read num               #get number
+  sum=$((sum + num))     #sum+=num
+  i=$((i + 1))
+done
+
+avg=$(echo $sum / $N | bc -l)
+
+echo $avg
