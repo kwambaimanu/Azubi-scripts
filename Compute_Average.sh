bd0a31738fe37feea965a0104d6eca23944c4330
kwambaimanu@gmail.com <kwambaimanu@gmail.com>
2022-05-20 09:51:11 +0300

    Compute average

---
+++ Compute_Average.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+#shell script to find average of n numbers
+  
+echo "Enter Size(N)"
+read N
+
+i=1
+sum=0
+
+echo "Enter Numbers"
+while [ $i -le $N ]
+do
+  read num               #get number
+  sum=$((sum + num))     #sum+=num
+  i=$((i + 1))
+done
+
+avg=$(echo $sum / $N | bc -l)
+
+echo $avg
